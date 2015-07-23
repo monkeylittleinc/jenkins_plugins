774870091cfe6b6c24dbf9287d9cb25c8107ce33
John Turner <john.turner@monkeylittle.com>
2015-07-23 10:11:03 +0100

    Update version and changelog.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@monkeylittle.com'
 license          'MIT'
 description      'Installs/Configures jenkins_plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '1.2.1'
 
 depends 'jenkins'
 depends 'maven'
