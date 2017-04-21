3bf8653e2b9145decf8975353703950ff672d57a
Stefan Wendler <stefan.wendler@mglug.de>
2017-04-21 11:59:46 +0200

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@monkeylittle.com'
 license          'MIT'
 description      'Installs/Configures jenkins_plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.0'
+version          '1.4.0'
 
 depends 'jenkins'
 depends 'maven', '4.0.1'
